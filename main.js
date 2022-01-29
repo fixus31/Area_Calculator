b15f0ee3c3d08539b7b1206e431f02b6181f0b04
George M <69652018+fixus31@users.noreply.github.com>
2022-01-29 17:14:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,17 +9,39 @@ const triangleCalc = (b, h) => {
     }
 }
 
+const rectCalc = (l, w) => {
+
+    result = l * w
+    return {
+        result: result
+
+    }
+}
+
 
 document.querySelector('.push').onclick = function (el) {
     el.preventDefault()
 
     let base = document.querySelector('.b').value,
         height = document.querySelector('.h').value
+        
 
     let answer = triangleCalc(base, height)
+   
 
     console.log(answer.result)
    
     document.querySelector('.result').innerText = answer.result
+ 
+}
 
+document.querySelector('.press').onclick = function (e) {
+    e.preventDefault()
+
+    let width = document.querySelector('.w').value,
+        length = document.querySelector('.l').value
+
+    let rec_res = rectCalc(width, length)
+
+    document.querySelector('.resrect').innerText = rec_res.result
 }
